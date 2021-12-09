e05df8bc965f4ab0f072d4d9b843f5482c2a8f03
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2021-12-09 14:22:15 -0500

    Update pipe.R

--- R/pipe.R
+++ R/pipe.R
@@ -1,14 +1,3 @@
-`%>>>>%` <- function(lhs, rhs) {
-  lhs <- substitute(lhs)
-  rhs <- substitute(rhs)
-  kind <- 1L
-  env <- parent.frame()
-  lazy <- TRUE
-  .External2(magrittr_pipe)
-}
-
-`%>%` <- function(x,f) do.call(f,list(x))
-
 #' Check pipe operator
 #' @export
 #' @rdname piperoot
