5bfff389151a1b072b6b45f89c7a4de13fbf5b32
J. Lucas McKay PhD MSCR <j.lucas.mckay@gmail.com>
2021-12-09 14:15:44 -0500

    Update pipe.R

--- R/pipe.R
+++ R/pipe.R
@@ -1,6 +1,10 @@
 `%>%` <- function(lhs, rhs) {
   lhs <- substitute(lhs)
   rhs <- substitute(rhs)
+  kind <- 1L
+  env <- parent.frame()
+  lazy <- TRUE
+  .External2(magrittr_pipe)
 }
 
 #' Check pipe operator
